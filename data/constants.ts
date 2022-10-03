82856b1792e2f3afb72646f399d8585c5003a182
jennifer <cxxyao2@gmail.com>
2022-10-02 23:07:13 -0400

    change backend

--- data/constants.ts
+++ data/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'http://192.168.2.10:5000/api/'
-//export const BACKEND_URL = 'https://backend-jennifer.herokuapp.com/api/'
+//export const BACKEND_URL = 'http://192.168.2.10:5000/api/'
+export const BACKEND_URL = 'https://backend-jennifer.herokuapp.com/api/'
 export const TOKEN_HEADER_NAME = 'my-x-xsrf-token'
 export const COOKIE_NAME = 'my-xsrf-token'
--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
